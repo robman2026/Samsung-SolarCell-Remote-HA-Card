848ba52668b108fc2e7a2bbbcbdc9ba5624e4a52
robman2026 <mywerbungonline@gmail.com>
2026-04-12 08:03:32 +0200

    Add filter to Netflix and YouTube button styles

--- samsung-solar-remote.js
+++ samsung-solar-remote.js
@@ -215,6 +215,7 @@ const STYLES = `
   .btn-netflix-logo {
     width:75%; height:75%;
     background: url("https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg") center/contain no-repeat;
+    filter: brightness(0) invert(0);
   }
 
   .btn-youtube {
@@ -224,6 +225,7 @@ const STYLES = `
   .btn-youtube-logo {
     width:75%; height:75%;
     background: url("https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg") center/contain no-repeat;
+    filter: brightness(0) invert(0);
   }
   
   .btn-prime {
